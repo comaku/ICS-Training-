c27c3e45c525f0453dd997e1416f4f9bcd829df3
comaku <136643530+comaku@users.noreply.github.com>
2026-09-11 08:11:31 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Project 1 Configuration
+|--------------------------------------------------------------------------
+| IMPORTANT:
+| Never upload this file to GitHub or expose it publicly.
+|--------------------------------------------------------------------------
+*/
+
+define('SUPABASE_URL', 'YOUR_SUPABASE_PROJECT_URL');
+define('SUPABASE_ANON_KEY', 'YOUR_SUPABASE_ANON_KEY');
+
+?>
