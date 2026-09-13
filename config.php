65d47accb54f36e22adbf0c0ea37ab3d1a988562
comaku <136643530+comaku@users.noreply.github.com>
2026-09-13 18:44:48 +0100

    Update config.php

--- config.php
+++ config.php
@@ -10,6 +10,6 @@
 */
 
 define('SUPABASE_URL', 'https://fmscowzyjarjbtkrtlfi.supabase.co');
-define('SUPABASE_ANON_KEY', 'fmscowzyjarjbtkrtlfi');
+define('SUPABASE_ANON_KEY', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtc2Nvd3p5amFyamJ0a3J0bGZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwODE1MTUsImV4cCI6MjEwNDY1NzUxNX0.SI-F7FZ0sgbdjIT0YuNMciW5O4RHTa7NiO-iSQzZq0A');
 
 ?>
